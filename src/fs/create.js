e649bff820531718dba0edbd4c839e84569107ef
foxnorth228 <khitrii03@gmail.com>
2022-05-31 18:42:20 +0300

    feat: implement function into fs/create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,30 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
 export const create = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    fs.open(path.join(__dirname, "files/fresh.txt"), "wx", (err, fd) => {
+        if(err) {
+            if(err.code == "EEXIST") {
+                err.message = "FS operation failed";
+            }
+            throw err;
+        }
+
+        try {
+            fs.write(fd, "I am fresh and young", (err, written, string) => {
+                if(err) {
+                    throw err;
+                }
+            });
+        } finally {
+            fs.close(fd, (err) => {
+                if (err) {
+                    throw err;
+                }
+            });
+        }
+    });
 };
