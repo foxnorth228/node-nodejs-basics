fc77767dbd29826b79a6fd938b59a598ba085daf
foxnorth228 <khitrii03@gmail.com>
2022-06-01 17:30:44 +0300

    feat: implement fs/list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,22 @@
+import * as fs from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export const list = async () => {
-    // Write your code here 
+    const files = join(__dirname, "files");
+    try {
+        const dir = await fs.opendir(files);
+        for await (const direct of dir) {
+            console.log(direct.name);
+        }
+    } catch(err) {
+        if(err.code === "ENOENT") {
+            const err = new Error("FS operation failed");
+            throw err;
+        } else {
+            throw err;
+        }
+    }
 };
+list();
