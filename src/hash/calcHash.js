957b560bcdffc099622e5a6edb083e9a2c1b3c0e
foxnorth228 <102675886+foxnorth228@users.noreply.github.com>
2022-06-03 14:46:53 +0300

    feat: implement crypto/hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,10 @@
+import "crypto";
+import "fs" as fs;
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const hashSum = crypto.createHash('sha256');
+    await fs.readFile("./files/fileToCalculateHashFor.txt", (err, data) => {
+        hashSum.update(data);
+    })
+    return hashSum.digest('hex');
 };
