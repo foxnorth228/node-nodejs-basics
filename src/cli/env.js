34ff7074b7d109ec0273d530fc6d5921382165e2
foxnorth228 <102675886+foxnorth228@users.noreply.github.com>
2022-06-03 13:58:22 +0300

    feat: implement cli/env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,11 @@
+import { env } from "process";
+
 export const parseEnv = () => {
-    // Write your code here 
+    let str = "";
+    for(const name in env) {
+        if(name.startsWith("RSS_")) {
+            str += `${name}=${env[name]}; `;
+        }
+    }
+    console.log(str.slice(0, -2));
 };
