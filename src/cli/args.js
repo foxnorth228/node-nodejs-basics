cf48f9c4eb0eaea43ebb498eead0c8baf8de604f
foxnorth228 <102675886+foxnorth228@users.noreply.github.com>
2022-06-03 14:13:41 +0300

    feat: implement cli/args function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,10 @@
+import { argv } from "process";
+
 export const parseArgs = () => {
-    // Write your code here 
+    const arg = argv.slice(2);
+    let str = "";
+    for(let i = 0; i < arg.length; i+=2) {
+        str += `${arg[i].slice(2)} is ${arg[i + 1]}, `;
+    }
+    console.log(str.slice(0, -2));
 };
