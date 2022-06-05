341afbf9ee9953f1a207363fba58b40f23306295
foxnorth228 <102675886+foxnorth228@users.noreply.github.com>
2022-06-05 15:36:05 +0300

    feat: implement wt/worker function

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,18 @@
-// n should be received from main thread
+import 'worker_threads' as worker;
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    if(worker.isMainThread) {
+        const worker1 = new worker.Worker(__filename, { workerData: { n: 17 }});
+        worker1.on('message', (msg) => {
+            console.log(msg);
+        });
+    } else {
+        parentPort.postMessage(nthFibonacci(workerData.n));
+    }
 };
